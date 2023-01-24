b93f51873a87944185d4f38873fed4dfe6507563
alexiadltg <a22aledelgar@inspedralbes.cat>
2023-01-24 09:50:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Deheroes-android
---
+++ android/src/org/grup7/deheroes/AndroidLauncher.java
@@ -0,0 +1,15 @@
+package org.grup7.deheroes;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new MyGdxGame(), config);
+    }
+}
